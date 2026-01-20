0f9a4bbe4dc6a4d4560d359cea8a08aeb2b52a5a
Daniel Baccarin <danielbaccarin@outlook.com>
2026-01-20 20:14:47 -0300

    Initial commit

---
+++ TransactionApp/TransactionApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using TransactionApp;
+
+
+Console.WriteLine("Running container...");
+
+MongoClientSettings settings = MongoClientSettings.FromConnectionString("mongodb://localhost:27018");
+
+MongoClient client = new MongoClient(settings);
+
+IMongoCollection<Transaction> transactionsCollection = client.GetDatabase("local").GetCollection<Transaction>("transaction");
+
+var filter = Builders<Transaction>.Filter.Empty;
+
+var transactions = await transactionsCollection.Find(filter).ToListAsync();
+
+foreach (var transaction in transactions)
+{
+    Console.WriteLine(transaction.Id);
+}
---
+++ TransactionApp/TransactionApp/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionApp
+{   
+    [BsonIgnoreExtraElements]
+    public class Transaction
+    {
+        [BsonId]
+        [BsonElement("_id")]
+        public Guid Id { get; set; }
+    }
+}
